a19eb7cd05896dbff6f12fa7af5086ea8edfaa9c
Alexandro de Oliveira <alex@artedigital.me>
2015-11-16 20:03:01 -0200

    Reverting last change

--- js/base64.js
+++ js/base64.js
@@ -17,18 +17,18 @@
         	} catch(e) {
         		alert(errorMessage(e));
         	};
-        	
-// ==> Assigning the HTML5 Form DOM Objects to variables:
-         var input = document.getElementById('base64');
-         var output = document.getElementById('output');
-         var textArea = input[0].value;
-         var radio = input.elements.choose.value;
+     
          
 // ==> Defining the Main Function, which is called by the "Submit" button through Global Event Handler "onClick()" :
 
         }
         function convert64() {
-                 
+// ==> Assigning the HTML5 Form DOM Objects to variables:
+         var input = document.getElementById('base64');
+         var output = document.getElementById('output');
+         var textArea = input[0].value;
+         var radio = input.elements.choose.value;
+// ==> The statement:                 
                  output.innerHTML = (radio !== 'encode') ? decode(textArea): encode(textArea);
 
 // ==> Old and tradicional toggle. Replaced by a more compact version:
