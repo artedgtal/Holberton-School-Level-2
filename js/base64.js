20207f5e09f101622db0e586d7c708c3f9377620
Alexandro de Oliveira <alex@artedigital.me>
2015-11-16 19:49:22 -0200

    Adding comments

--- js/base64.js
+++ js/base64.js
@@ -1,14 +1,14 @@
 
-
+// ==> Function to show a fancy error message;
          function errorMessage(o){
          	return o.toString().split(':')[2].slice(1)+'\nPlease, verify it and try again ;-)';
         }
-      
+// ==> The two basic functions, Decode/Encode      
         function decode(s) {
         	try {
             return atob(s);
         	} catch(e){
-        		alert(errorMessage(e));
+        		alert(errorMessage(e)); // ==> Using the function above
         	};
         }
         function encode(s) {
@@ -17,16 +17,24 @@
         	} catch(e) {
         		alert(errorMessage(e));
         	};
+        	
+// ==> Assigning the HTML5 Form DOM Objects to variables:
+         var input = document.getElementById('base64');
+         var output = document.getElementById('output');
+         var textArea = input[0].value;
+         var radio = input.elements.choose.value;
+         
+// ==> Defining the Main Function, which is called by the "Submit" button through Global Event Handler "onClick()" :
+
         }
         function convert64() {
+                 
+                 output.innerHTML = (radio !== 'encode') ? decode(textArea): encode(textArea);
 
-            var input = document.getElementById('base64');
-            var output = document.getElementById('output');
-            var textArea = input[0].value;
-            var radio = input.elements.choose.value;
-            if(radio !== 'encode') {
+// ==> Old and tradicional toggle. Replaced by a more compact version:
+         /*   if(radio !== 'encode') {
             	output.innerHTML = decode(textArea);
             } else {
             	output.innerHTML = encode(textArea);
-            }
+            }*/
         }
