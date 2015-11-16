0257f9e2d134297ea52cfe669940583cb9825d94
Alexandro de Oliveira <alex@artedigital.me>
2015-11-16 19:35:02 -0200

    Compactness

--- js/behavior.js
+++ js/behavior.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var thumbnailElement = document.getElementById("smart_thumbnail");
     thumbnailElement.addEventListener("click", function() {
-        if(this.className == ''){
-            this.className = 'small';
-        } else {this.className = '';}
+        this.className = (this.className == '') ? 'small' : '' ;
+
 });
 
 });
